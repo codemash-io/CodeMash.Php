f3f8f83c0a1436b37f086c2532f772b1beab93cb
Mindaugas Jackūnas <mindaugas@jackunas.lt>
2020-12-27 13:41:23 +0200

    Move in feature tests

---
+++ tests/Feature/FakerSingleton.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Faker\Factory;
+use Faker\Generator;
+
+final class FakerSingleton
+{
+    /**
+     * @var null|Generator
+     */
+    private static $instance;
+
+    public static function getInstance(): Generator
+    {
+        if (is_null(self::$instance)) {
+            echo '*setup*';
+            self::$instance = Factory::create();
+        }
+
+        return self::$instance;
+    }
+}
